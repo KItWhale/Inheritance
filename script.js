472adee7a43654b897832decd4df20e65a64d2ec
Your Name <you@example.com>
2018-10-07 13:15:37 +0400

    Lesson2

---
+++ script.js
@@ -0,0 +1,14 @@
+var obj = {
+    "first_name": "Tigran",
+    "last_name": "Kisibekyan",
+    "age": 14,
+    "tumo_student": true,
+
+    sayHello(){
+        console.log("Hello");
+    }
+}
+
+console.log(obj);
+console.log(obj.first_name);
+obj.sayHello();
